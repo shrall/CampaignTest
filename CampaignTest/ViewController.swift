6dd148d00e010f48dd46356fc456ea68294d4f42
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-06-17 11:53:34 +0700

    Initial Commit

---
+++ CampaignTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampaignTest
+//
+//  Created by Marshall Kurniawan on 17/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
